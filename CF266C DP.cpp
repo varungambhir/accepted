d38f8a5f7c35bec307a6cb6dc88aeedcccb10f96
Ashish Sareen <ashish.sareen95@gmail.com>
2016-02-09 23:08:13 +0530

    Ladder

---
+++ CF266C DP.cpp
@@ -0,0 +1,95 @@
+/*
+Written by : Ashish Sareen
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+#define FOR(i,a,n) for(int (i) = (a); (i) <= (n) ; ++(i))
+#define ROF(i,a,n) for(int (i)=(a);(i) >= (n); --(i))
+#define SD(x) scanf("%d",&x)
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define L(x) (x<<1)
+#define R(x) (x<<1|1)
+#define repstl(v) for(__typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
+#define endl "\n"
+#define INPFILE freopen("input.in","r",stdin)
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define present_mapset(c,x) ((c).find(x) != (c).end())
+#define present_vector(c,x) (find(c.begin(),e.end(),x) != (c).end())
+#define TRACE
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+ /*Tired of lying in the sunshine, Staying home to watch the rain
+You are young and life is long, And there is time to kill today
+And then one day you find, 10 years have got behind you
+No one told you when to run, You missed the starting gun
+- Time, Pink Floyd*/
+
+#define MAXN 500050
+#define MOD 1000000007
+
+ll a[MAXN];
+ll sum[MAXN];
+ll cnt[MAXN];
+ll cntsum[MAXN];
+int main(int argc, char const *argv[])
+{
+    BOOST;
+    ll n;
+    cin >> n;
+    sum[0] = 0;
+    FOR(i,1,n)
+    {
+        cin >> a[i];
+        sum[i] = a[i] + sum[i-1];
+    }
+
+    if(sum[n] % 3 != 0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    FOR(i,1,n)
+    if(sum[n]-sum[i-1] == sum[n]/3)
+        cnt[i] = 1;
+    else
+        cnt[i] = 0;
+
+    cntsum[n] = cnt[n];
+    ROF(i,n-1,1)
+    {
+        cntsum[i] = cnt[i] + cntsum[i+1];
+    }
+
+    ll ans = 0;
+    FOR(i,1,n-2)
+    {
+        if(sum[i] == sum[n]/3)
+        {
+            ans += cntsum[i+2];
+        }
+    }
+
+    cout << ans << endl;
+
+
+    return 0;
+}
