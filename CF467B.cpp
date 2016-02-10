ba42be66bd070cf2bd4dc6437281ec946ed690ac
Ashish Sareen <ashish.sareen95@gmail.com>
2016-02-10 16:03:45 +0530

    Birthday Commit

---
+++ CF467B.cpp
@@ -0,0 +1,93 @@
+/*
+Written by : Ashish Sareen
+*/
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+#define FOR(i,a,n) for(int (i) = (a); (i) <= (n) ; ++(i))
+#define ROF(i,a,n) for(int (i)=(a);(i) >= (n); --(i))
+#define SD(x) scanf("%d",&x)
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define L(x) (x<<1)
+#define R(x) (x<<1|1)
+#define repstl(v) for(__typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
+#define endl "\n"
+#define INPFILE freopen("input.in","r",stdin)
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define present_mapset(c,x) ((c).find(x) != (c).end())
+#define present_vector(c,x) (find(c.begin(),e.end(),x) != (c).end())
+#define TRACE
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+ /*Tired of lying in the sunshine, Staying home to watch the rain
+You are young and life is long, And there is time to kill today
+And then one day you find, 10 years have got behind you
+No one told you when to run, You missed the starting gun
+- Time, Pink Floyd*/
+
+#define MAXN 500050
+#define MOD 1000000007
+
+ll pow(ll a,ll b)
+{
+    ll res = 1;
+    while(b > 0)
+    {
+        if(b&1)
+            res *= a;
+        a *= a;
+        b = b >> 1;
+    }
+    return res;
+}
+
+ll countsetbit(ll n)
+{
+    ll cnt = 0;
+    while(n)
+    {
+        cnt++;
+        n = n&(n-1);
+    }
+    return cnt;
+}
+
+ll arr[10000];
+int main(int argc, char const *argv[])
+{
+    BOOST;
+    ll n , m , k , our;
+    cin >> n >> m >> k;
+    FOR(i,1,m+1)
+        cin >> arr[i];
+
+    ll limit = pow(2,n);
+    ll x,cnt = 0;
+    FOR(i,1,m)
+    {
+        x = arr[i] ^ arr[m+1];
+        cnt = cnt +  (countsetbit(x) <=k ? 1 : 0 );
+    }
+
+    cout << cnt << endl;
+
+
+    return 0;
+}
