2951b9fe7530b7ac4e94b65ed35acf259344cc6c
Ashish Sareen <ashish.sareen95@gmail.com>
2016-02-14 16:45:01 +0530

    V

---
+++ CF259A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+#define FOR(i,a,n) for(int (i) = (a); (i) <= (n) ; ++(i))
+#define ROF(i,a,n) for(int (i)=(a);(i) >= (n); --(i))
+#define SD(x) scanf("%d",&x)
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define L(x) (x<<1)
+#define R(x) (x<<1|1)
+#define repstl(v) for(__typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
+#define endl "\n"
+#define INPFILE freopen("input.in","r",stdin)
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define present_mapset(c,x) ((c).find(x) != (c).end())
+#define present_vector(c,x) (find(c.begin(),e.end(),x) != (c).end())
+#define TRACE
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+ /*Tired of lying in the sunshine, Staying home to watch the rain
+You are young and life is long, And there is time to kill today
+And then one day you find, 10 years have got behind you
+No one told you when to run, You missed the starting gun
+- Time, Pink Floyd*/
+
+
+double modulo(double a, int b)
+{
+    double x=1,y=a;
+    
+    while(b>0)
+    {
+        if(b%2==1)
+        {
+            x = x*y;
+        }
+        
+        y = (y*y);
+        b=b/2;
+    }
+    return x;
+}
+
+int main(int argc, const char * argv[])
+{
+    BOOST;
+    double sum,m,n,i;
+    sum=0;
+    
+    cin>>m>>n;
+    
+    for(i=1;i<=m;i++)
+    {
+        sum = sum + i*(modulo(i/m,n)-modulo((i-1)/m,n));
+    }
+    
+    cout<<fixed;
+    cout<<setprecision(5)<<sum<<"\n";
+    
+    return 0;
+}
---
+++ Hackerrank Cut the Tree.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+#define FOR(i,a,n) for(int (i) = (a); (i) <= (n) ; ++(i))
+#define ROF(i,a,n) for(int (i)=(a);(i) >= (n); --(i))
+#define SD(x) scanf("%d",&x)
+#define eb emplace_back
+#define mp make_pair
+#define F first
+#define S second
+#define L(x) (x<<1)
+#define R(x) (x<<1|1)
+#define repstl(v) for(__typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
+#define endl "\n"
+#define INPFILE freopen("input.in","r",stdin)
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define present_mapset(c,x) ((c).find(x) != (c).end())
+#define present_vector(c,x) (find(c.begin(),e.end(),x) != (c).end())
+#define TRACE
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+ /*Tired of lying in the sunshine, Staying home to watch the rain
+You are young and life is long, And there is time to kill today
+And then one day you find, 10 years have got behind you
+No one told you when to run, You missed the starting gun
+- Time, Pink Floyd*/
+
+ll val[1000000]={0};
+int vis[1000000]={0};
+
+std::vector<ll> G[1000000];
+ll W = 0;
+ll minii = INT_MAX;
+
+ll dfs(ll root)
+{   
+    if(vis[root])
+        return 0LL;
+
+    vis[root] = 1;
+    
+    ll c = val[root];
+
+    repstl(G[root])
+    {
+        c += dfs(*it);
+    }
+
+    minii = min(minii,abs(W -c -c));
+
+    return c;
+}
+
+int main(int argc, char const *argv[])
+{
+    BOOST;
+    ll n;
+    ll a,b;
+    cin >> n;
+    W = 0;
+    FOR(i,1,n)
+    {
+        cin >> val[i];
+        W += val[i];
+    }
+
+    FOR(i,1,n-1)
+    {
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    ll u = dfs(1);
+
+    cout << minii << endl;
+
+    return 0;
+}
