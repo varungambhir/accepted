507496cec834b038c28c2d3e983a0659c18d0187
Ashish Sareen <ashish.sareen95@gmail.com>
2016-02-22 17:48:06 +0530

    python template

---
+++ CF630H.py
@@ -0,0 +1,6 @@
+from sys import stdin,stdout
+from math import factorial as fact
+inp= map(int , stdin.readline().rstrip().split())
+n = inp[0]
+ans = (n*(n-1)*(n-2)*(n-3)*(n-4))*(n*(n-1)*(n-2)*(n-3)*(n-4))/120
+stdout.write(str(ans) + "\n")  
