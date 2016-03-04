9c930c8dbd56029e6d1a9b457a986e2d1773e36d
Ashish Sareen <ashish.sareen95@gmail.com>
2016-03-05 03:32:17 +0530

    QSET SegTree

--- QSET.cpp
+++ QSET.cpp
@@ -1,188 +1,240 @@
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <bitset>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <cstdio>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <cstring>
-#include <climits>
-#include <cctype>
-#include <cassert>
-#include <iterator>
-#include <complex>
-//#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ull unsigned long long
-#define ll long long int
-#define pii pair<int,int>
+typedef  long long int ll;
+#define FOR(i,a,n) for(int (i) = (a); (i) <= (n) ; ++(i))
+#define ROF(i,a,n) for(int (i)=(a);(i) >= (n); --(i))
+#define SD(x) scanf("%d",&x)
+#define eb emplace_back
 #define pb push_back
 #define mp make_pair
-#define F(i,a,n) for(int i=(a);i<(n);++i)
-#define FF(i,a,n) for(i=(a);i<(n);++i)
-#define REP(i,a,n) for(i=(a);i<=(n);++i)
-#define FD(i,a,n) for(int i=(a);i>=(n);--i)
-#define FE(it,x) for(it=x.begin();it!=x.end();++it)
-#define V(x) vector<x>
-#define S(x) scanf("%d",&x)
-#define Sl(x) scanf("%lld",&x)
-#define M(x,i) memset(x,i,sizeof(x))
-#define repVector(v)  for( typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
-#define repSet(s) for( set<ll>::iterator it = s.begin(); it != s.end(); it++ )
-#define debug(i,sz,x) F(i,0,sz){cout<<x[i]<<" ";}cout<<endl
-#define matrix vector< vector<ll> >
+#define F first
+#define S second
+#define L(x) (x<<1)
+#define R(x) (x<<1|1)
+#define repstl(v) for(__typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
+#define endl "\n"
+#define INPFILE freopen("input.in","r",stdin)
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define present_mapset(c,x) ((c).find(x) != (c).end())
+#define present_vector(c,x) (find(c.begin(),e.end(),x) != (c).end())
+/*Tired of lying in the sunshine, Staying home to watch the rain
+You are young and life is long, And there is time to kill today
+And then one day you find, 10 years have got behind you
+No one told you when to run, You missed the starting gun
+- Time, Pink Floyd*/
+#define MAXN 100010
+#define MOD 1000000007
+
 #define gc getchar_unlocked
-#define ft first
-#define se second
-#define MAXNN 10000010
-#define mod 10000007
-inline void inputfile() {
-#ifndef ONLINE_JUDGE
-    freopen("input.in","r",stdin);
-#endif
-}
- 
-void scanint(int &x)
+template <typename T>
+void scanint(T &x)
 {
     register int c = gc();
     x = 0;
     int neg = 0;
     for(;((c<48 || c>57) && c != '-');c = gc());
-    if(c=='-') {neg=1;c=gc();}
+        if(c=='-') {neg=1;c=gc();}
     for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
-    if(neg) x=-x;
+        if(neg) x=-x;
 }
-void scanintll(long long int &x)
+
+typedef pair<int,int> pii;
+struct node
 {
-    register int c = gc();
-    x = 0;
-    int neg = 0;
-    for(;((c<48 || c>57) && c != '-');c = gc());
-    if(c=='-') {neg=1;c=gc();}
-    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
-    if(neg) x=-x;
-}
-int segment_tree[MAXNN][3],lazzzy[MAXNN][3],stree[MAXNN][3], srr[MAXNN];
-int build_tree(int,int,int);
-int update_tree(int,int,int,int,int,int);
-int query_tree(int,int,int,int,int,int);
-char strr[100010];
-int main()
-{
-//ios_base::sync_with_stdio(false);
-  inputfile();
-  ll op;
- int ind,left,kk,right,i,n,m;
-scanint(n);
-scanint(m);
-scanf("%s",strr);
-srr[0]=0;
-FF(i,1,(n+1))
-srr[i]=srr[i-1]+((int)strr[i-1]-'0')%3;
-FF(i,0,3)
-build_tree(1,0,n);
-while(m--)
-{
-    scanint(ind);
-    scanint(left);
-    scanint(right);
-    if(ind==1)
+    int l, r ;
+    ll cnt;
+    int totalsum , suffix[3] , prefix[3] , lazy;
+    node()
     {
-        i=(3+right%3 - srr[left]%3)%3;
-        update_tree(1,0,n,i,left,n);
+        FOR(i,0,2) suffix[i] = prefix[i]  = 0;
+        lazy = cnt = totalsum = 0;
+    }
+};
+
+struct node segtree[MAXN<<3];
+int arr[MAXN<<3];
+int maxi = -1;
+void error(int index)
+{
+    node &p = segtree[index];
+    printf("L: %d R: %d Totalsum: %d cnt: %lld prefix[ %d %d %d ] suffix[ %d %d %d ] ", 
+           p.l,p.r,p.totalsum,p.cnt,p.prefix[0],p.prefix[1],p.prefix[2],p.suffix[0],p.suffix[1],p.suffix[2]
+           );
+      if(p.l == p.r)
+        printf("Val: %d",arr[p.l]);
+    printf("\n");
+}
+
+
+void lazyme(int index = 1)
+{
+    return;
+}
+void merge(node &ptr,const node &left,const node &right)
+{
+    ptr.cnt = left.cnt + right.cnt;
+    FOR(i,0,2)
+    FOR(j,0,2)
+    if(  (i+j)%3 == 0 )
+        ptr.cnt += ((ll)left.suffix[i] * (ll)right.prefix[j]);
+
+    FOR(i,0,2)
+    {
+        ptr.prefix[i] = left.prefix[i] + right.prefix[ (6 +i -left.totalsum )%3 ];
+        ptr.suffix[i] = right.suffix[i] + left.suffix[ (6 +i -right.totalsum)%3 ];
+    }
+
+    ptr.totalsum = (left.totalsum + right.totalsum)%3;
+}
+
+void assign(int index , int p)
+{
+    node ptr;
+    ptr.prefix[p] = 1;
+    ptr.suffix[p] = 1;
+    ptr.totalsum = p;
+    if(!p) //  p == 0
+        ptr.cnt = 1;
+    ptr.l = segtree[index].l;
+    ptr.r = segtree[index].r;
+
+    segtree[index] = ptr;
+}
+
+void build_tree(pair<int,int> p, int index = 1)
+{
+
+    struct node &ptr = segtree[index];
+    ptr.l = p.F;
+    ptr.r = p.S;
+    ptr.lazy = 0;
+
+    if(p.F == p.S)
+    {
+        assign(index,arr[p.F]%3);
+        return; 
     }
     else
     {
-        op=0;
-        FF(i,0,3)
-        {
-            kk=query_tree(1,0,n,i,left-1,right);
-            op=op+ (kk*(kk-1))/2;
-        }
-        printf("%lld\n",op);
- 
+        int mid = (p.F + p.S)>>1;
+        build_tree(mp(p.F,mid),L(index));
+        build_tree(mp(mid+1,p.S),R(index));
+
+        merge(segtree[index],segtree[L(index)],segtree[R(index)]);
     }
- 
+    //maxi = max(maxi,index);
+    //error(index);
 }
-  return 0;
-}
- 
-int build_tree(int n,int a,int b)
+
+struct node query(int lo, int hi, int index = 1)
 {
-    if(a==b)
+
+    if(lo > hi || segtree[index].l > segtree[index].r)
     {
-        F(i,0,3)
-            segment_tree[n][i]=lazzzy[n][i]=0;
-            segment_tree[n][srr[a]%3]++;
-            return 0;
+        node temp;
+        return temp;
+    }    
+    struct node &ptr = segtree[index];
+    //lazyme(index);
+
+    if(lo <= ptr.l && ptr.r <= hi)
+    {
+        return segtree[index];
     }
- 
-int m=a+(b-a)/2;
-build_tree(2*n,a,m);
-build_tree(2*n+1,m+1,b);
-F(i,0,3){
-lazzzy[n][i]=0;
-segment_tree[n][i]=segment_tree[2*n][i]+segment_tree[2*n+1][i];
+
+    int mid = (ptr.l + ptr.r) >> 1;
+
+    if(mid < lo)
+    {
+        return query(lo,hi,R(index));
+    }
+    if( hi <= mid)
+    {
+        return query(lo,hi,L(index));
+    }
+
+    node l = query(lo,hi,L(index));
+    node r = query(lo,hi,R(index));
+    node p;
+    merge(p,l,r);
+    p.l = l.l;
+    p.r = r.r;
+    return p;
+
 }
-return 0;
-}
-int update_tree(int n,int a,int b,int k,int x,int y)
+
+void update(int lo,int hi , int val , int index = 1)
 {
-if(y<a || x>b)
-        return 0;
-if(y>=b && a>=x)
-{
-        F(i,0,3)
+    node &ptr = segtree[index];
+    //lazyme(index);
+    //out of range
+    if( ptr.l > hi || ptr.r < lo || lo > hi )
+        return;
+
+    if(lo <= ptr.l && ptr.r <= hi)
+    {
+        if(ptr.l != ptr.r)
         {
-            stree[n][i]=segment_tree[n][i];
+            //lazy propagation
+            update(lo,hi,val,L(index));
+            update(lo,hi,val,R(index));
+            merge(segtree[index],segtree[L(index)],segtree[R(index)]);
+            return;
         }
-        F(i,0,3)
+        else
         {
-            segment_tree[n][i]=stree[n][(k+i+lazzzy[n][i])%3];
+            assign(index,val%3);
+            return;
         }
-        F(i,0,3)
+    }
+    else
+    {
+        update(lo,hi,val,L(index));
+        update(lo,hi,val,R(index));
+        merge(segtree[index],segtree[L(index)],segtree[R(index)]);
+    }
+
+}
+char s[1000100];
+int main(int argc, char const *argv[])
+{
+    //BOOST;
+    int n,m;
+    scanint(n);
+    scanint(m);
+    scanf("%s",s);
+    FOR(i,1,n)
+    {
+        arr[i] = s[i-1] - '0';
+    }
+
+    build_tree(make_pair(1,n),1);
+    while(m--)
+    {
+        int c,x, y;
+        scanint(c);
+        scanint(x);
+        scanint(y);
+        //cerr << "---------\n";
+        if(c == 1)
         {
-            lazzzy[2*n][i]=lazzzy[2*n][i]+(lazzzy[n][i]+k)%3;
-            lazzzy[2*n+1][i]=lazzzy[2*n+1][i]+(lazzzy[n][i]+k)%3;
-            lazzzy[n][i]=0;
+            arr[x] = y;
+            update(x,x,y,1);
         }
-  return 0;
-}
-F(i,0,3)
-{
-    lazzzy[2*n][i]=lazzzy[2*n][i]+lazzzy[n][i]%3;
-    lazzzy[1+2*n][i]=lazzzy[1+2*n][i]+lazzzy[n][i]%3;
-    lazzzy[n][i]=0;
-}
-int mid=a+(b-a)/2;
-update_tree(2*n,a,mid,k,x,y);
-update_tree(1+2*n,mid+1,b,k,x,y);
-F(i,0,3)
-segment_tree[n][i]=segment_tree[2*n][i]+segment_tree[1+2*n][i];
-return 0;
-}
-int query_tree(int n,int a,int b,int ind,int x,int y)
-{
-if(a>y || x>b)
+        else
+        {
+            printf("%lld\n",query(x,y,1).cnt );
+        }
+        
+       /* 
+        FOR(i,1,maxi)
+        {
+            error(i);
+        }
+        */
+    }
+
+
+
     return 0;
-if( y>=b && a>=x)
-return segment_tree[n][ind];
- 
-int m=a+(b-a)/2;
-int k1=query_tree(2*n,a,m,ind,x,y);
-int k2=query_tree(2*n+1,m+1,b,ind,x,y);
- 
-return (k2+k1);
 }
