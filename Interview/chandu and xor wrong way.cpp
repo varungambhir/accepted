0501d91d085993586f16e50af6a06b3f88350ba9
Ashish Sareen <ashish.sareen95@gmail.com>
2016-03-12 00:03:51 +0530

    Simple

---
+++ Interview/chandu and xor wrong way.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef  long long int ll;
+#define FOR(i,a,n) for(int (i) = (a); (i) <= (n) ; ++(i))
+#define ROF(i,a,n) for(int (i)=(a);(i) >= (n); --(i))
+#define SD(x) scanf("%d",&x)
+#define eb emplace_back
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define L(x) (x<<1)
+#define R(x) (x<<1|1)
+#define repstl(v) for(__typeof(v.begin()) it = v.begin(); it != v.end(); it++ )
+#define endl "\n"
+#define INPFILE freopen("input.in","r",stdin)
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define present_mapset(c,x) ((c).find(x) != (c).end())
+#define present_vector(c,x) (find(c.begin(),e.end(),x) != (c).end())
+#define TRACE
+#ifdef TRACE
+#define trace1(x)                cerr << #x << ": " << x << endl;
+#define trace2(x, y)             cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z)          cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)       cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+#define trace5(a, b, c, d, e)    cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << endl;
+#define trace6(a, b, c, d, e, f) cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << " | " << #e << ": " << e << " | " << #f << ": " << f << endl;
+#else
+#define trace1(x)
+#define trace2(x, y)
+#define trace3(x, y, z)
+#define trace4(a, b, c, d)
+#define trace5(a, b, c, d, e)
+#define trace6(a, b, c, d, e, f)
+#endif
+
+ /*Tired of lying in the sunshine, Staying home to watch the rain
+You are young and life is long, And there is time to kill today
+And then one day you find, 10 years have got behind you
+No one told you when to run, You missed the starting gun
+- Time, Pink Floyd*/
+#define MAXN 3000005
+#define MOD 1000000007
+
+
+int a[1010][1010]={0};
+int rowsum[1010]={0};
+int colsum[1010]={0};
+
+int colone[1010][20]={0};
+int colzero[1010][20]={0};
+int rowone[1010][20]={0};
+int rowzero[1010][20]={0};
+
+
+int main(int argc, char const *argv[])
+{
+    BOOST;
+    int n,m;
+    cin >> n >> m;
+    FOR(i,1,n)
+    {
+        FOR(j,1,m)
+        {
+            cin >> a[i][j];
+            rowsum[i] += a[i][j];
+            colsum[j] += a[i][j];
+        }
+    }
+    
+    int ans = 0;
+    int sum;
+    FOR(i,1,n)
+    {
+        FOR(j,1,m)
+        {
+            sum =  rowsum[i]^colsum[j];
+            ans = max(ans,sum);
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
